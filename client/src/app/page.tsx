5b7061f7ba6a37d8035953ae242b05a281688a4e
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-08-20 07:24:07 +0530

    frontend setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello I am Het Patel.</h1>
+      <Button>Hello Next</Button>
+    </div>
+  );
+};
+
+export default App;
